1fef984b605e920f4553154f4f8f453e849905b4
JulianVelasquezBy2003 <julianjosevelasquezcoila@gmail.com>
2026-06-17 11:31:59 -0500

    IMP claseUsuario

--- src/main/java/modelo/Usuario.java
+++ src/main/java/modelo/Usuario.java
@@ -2,4 +2,46 @@ package modelo;
 
 public class Usuario {
     
+    // Atributos de la clase usuario
+    
+    private String nombre;
+    private String contraseña;
+    private String rol;
+    
+    // Constructores
+
+    public Usuario(String nombre, String contraseña, String rol) {
+        this.nombre = nombre;
+        this.contraseña = contraseña;
+        this.rol = rol;
+    }
+    
+    // GETTERS Y SETTERS
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    
+    
+    
 }
