d68be0394fb390c90c2267cb6f4890b9ffdccaaa
Kirumi_A <svon.yoi@gmail.com>
2026-06-19 12:43:41 -0500

    mio commit

--- src/main/java/com/mycompany/gestionderutas/MenuPrincipal.java
+++ src/main/java/com/mycompany/gestionderutas/MenuPrincipal.java
@@ -3,6 +3,7 @@ package com.mycompany.gestionderutas;
 public class MenuPrincipal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("━━━━━━━━━━━━━━━━━━━━ Menu ━━━━━━━━━━━━━━━━━━━━━━");
+        Usuario usuario = new Usuario ("Angel","hoola","Administrador");
     }
 }
